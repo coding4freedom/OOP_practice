12a80cd026be03de8cadc4b0eab8285af0ada05a
coding4freedom <chawkins2010@hotmail.com>
2022-04-23 23:59:32 -0500

    tank oop creation unfinished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
 
+/* using Constructor function to create obj  */
+
+
+function DestroyTarget(target, armor, wpn) {
+    this.target = target;
+    this.armor = armor;
+    this.wpn = { 
+        handgun: 'handgun',
+        ar: 'ar', 
+        shotgun: 'shotgun', 
+    };
+    let weapon = {
+        handgun: 10,
+        ar: 20,
+        shotgun: 45,
+    };
+
+    let checkTarget = function() {
+        if(target === 0)
+            alert('Target has been destroyed!')
+    };
+
+    this.atk = function() {
+
+    };
+
+    let calcDmg = function() {
+
+    };
+
+}
+
+const tank = new DestroyTarget(250,50)
